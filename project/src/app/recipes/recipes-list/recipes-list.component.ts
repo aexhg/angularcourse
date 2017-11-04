283176af5825d3126a9cc459286e2b6e9980c508
henri <henri.gough@gmail.com>
2017-11-04 16:54:48 +0000

    setup project recipe model list initial page

--- project/src/app/recipes/recipes-list/recipes-list.component.ts
+++ project/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      "Cookie Dough",
+      "Cookie Douugh", 
+      "http://www.seriouseats.com/images/2015/08/20150823-grilled-chicken-cutlet-recipe-kenji-01.jpg"
+    )
+  ]
   constructor() { }
 
   ngOnInit() {
--- project/src/app/recipes/recipes.component.ts
+++ project/src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      "Cookie Dough",
+      "Cookie Douugh", 
+      "http://www.seriouseats.com/images/2015/08/20150823-grilled-chicken-cutlet-recipe-kenji-01.jpg"
+    )
+  ]
   constructor() { }
 
   ngOnInit() {
