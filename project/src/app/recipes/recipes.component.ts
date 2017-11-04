dac80805cf23c6a5c0d22879a6c13915fa60c79d
henri <henri.gough@gmail.com>
2017-11-04 20:09:26 +0000

    do the shopping list edit component

--- project/src/app/recipes/recipes.component.ts
+++ project/src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -7,13 +6,7 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      "Cookie Dough",
-      "Cookie Douugh", 
-      "http://www.seriouseats.com/images/2015/08/20150823-grilled-chicken-cutlet-recipe-kenji-01.jpg"
-    )
-  ]
+  
   constructor() { }
 
   ngOnInit() {
